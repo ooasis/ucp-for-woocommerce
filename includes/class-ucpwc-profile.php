cecb8793d834d08e930822640e61d9af92e65f64
hsun <hang.s@taboola.com>
2026-09-06 15:36:33 -0700

    Use wp_json_encode for the JWK thumbprint

--- includes/class-ucpwc-profile.php
+++ includes/class-ucpwc-profile.php
@@ -22,7 +22,7 @@ class UCPWC_Profile
         $jwk = \UCPWC\Signatures\ec_pem_to_jwk($key, '');
         // kid = RFC 7638 thumbprint (lexicographic members crv,kty,x,y)
         $thumb = ['crv' => $jwk['crv'], 'kty' => $jwk['kty'], 'x' => $jwk['x'], 'y' => $jwk['y']];
-        $jwk['kid'] = \UCPWC\Signatures\b64url_encode(hash('sha256', json_encode($thumb, JSON_UNESCAPED_SLASHES), true));
+        $jwk['kid'] = \UCPWC\Signatures\b64url_encode(hash('sha256', wp_json_encode($thumb, JSON_UNESCAPED_SLASHES), true));
         update_option('ucpwc_signing_key', ['pem' => $pem, 'jwk' => $jwk], false);
     }
 
